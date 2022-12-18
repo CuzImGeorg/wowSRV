ed105882bf57e53352c26f852d4c1b26ffd72060
georg <georg>
2022-12-18 21:37:48 +0100

    dsa

--- src/main/resources/sql/charakter/inserts.sql
+++ src/main/resources/sql/charakter/inserts.sql
@@ -1,7 +1,17 @@
-INSERT INTO character (maxHp, ad, ap, klasse, name, description) VALUES (2000, 2, 80, 'TANK', 'KV-1S', 'DER KV-1S ist ein sowjetischer TANK der Stufe 6' );
+INSERT INTO character (maxHp, ad, ap, klasse, name, description) VALUES (4000, 2, 80, 'TANK', 'KV-1S', 'DER KV-1S ist ein sowjetischer TANK der Stufe 6' );
 INSERT INTO ability (name, aid, description, charid ) VALUES  ('Strimpack' , 0, 'Regenerates 2% missing heath every second', 1);
 INSERT INTO ability (name, aid, description, charid ) VALUES  ('Absorbing' , 1, 'GAIN 10% current helth shield cd 30 for 2.5 sec', 1);
 INSERT INTO ability (name, aid, description, charid ) VALUES  ('Ignite' , 2, 'DEAL 20 + 500% ap dmg per second for 5 seconds cd 10', 1);
 INSERT INTO ability (name, aid, description, charid ) VALUES  ('Hurt' , 3, ' DEAL 120 + ad damage cd 10', 1);
 INSERT INTO ability (name, aid, description, charid ) VALUES  ('Pump Up' , 4, 'Get 0.5% current Helth ap and ad for 10 sec cd 60', 1);
+
+INSERT INTO character (maxHp, ad, ap, klasse, name, description) VALUES (4000, 2, 80, 'TANK', 'T-90', 'Der T-90 ist ein Tank der russischen Armee, welcher vom Verteidigungsministerium der Russischen Föderation 1992 eingeführt wurde.' );
+INSERT INTO ability (name, aid, description, charid ) VALUES  ('Comming Late' , 0, 'GAIN 5 Ad and 1 ap extra every 5 min', 1);
+INSERT INTO ability (name, aid, description, charid ) VALUES  ('Granade' , 1, 'Deal 40 + 100% ap dmg to all 10', 1);
+INSERT INTO ability (name, aid, description, charid ) VALUES  ('Vampire' , 2, 'Deal 120 + ad dmg and heal for 10% dmg  cd 10', 1);
+
+
+INSERT INTO ability (name, aid, description, charid ) VALUES  ('Hurt' , 3, ' DEAL 120 + ad damage cd 10', 1);
+INSERT INTO ability (name, aid, description, charid ) VALUES  ('Pump Up' , 4, 'Get 0.5% current Helth ap and ad for 10 sec cd 60', 1);
+
 
